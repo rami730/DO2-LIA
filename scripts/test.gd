b3d363eab1781d21756e1b40f02ded1a6c4c9048
rami730 <rami_730@hotmail.com>
2024-05-22 18:00:16 +0200

    Bug fix

--- scripts/test.gd
+++ scripts/test.gd
@@ -3,6 +3,6 @@ extends Node
 #test environment
 func _ready():
 	#creates a new instance of Results class with parameters
-	var my_result = Results.new("Node 1", "Node 2", "Before", "2024-05-22 17:28")
+	var my_result = Results.new(1, 2, "Before", "2024-05-22 17:28")
 	my_result.print_all()
 	print(my_result.answer)
