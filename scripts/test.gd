5a19f42e8c57d6648a42d48d01974859e8b8c4bf
rami730 <rami_730@hotmail.com>
2024-05-23 16:42:36 +0200

    Added ability to read data from database and convert into class objects
    Use results_array, it has all the data

--- scripts/test.gd
+++ scripts/test.gd
@@ -1,8 +1,47 @@
 extends Node
 
+var database : SQLite
+var results_array : Array
+
 #test environment
-func _ready():
-	#creates a new instance of Results class with parameters
-	var my_result = Results.new(1, 2, "Before", "2024-05-22 17:28")
-	my_result.print_all()
-	print(my_result.answer)
+func _ready():	
+	#initilize database
+	database = SQLite.new()
+	database.path = "res://resources/test_db.db"
+	database.open_db()
+	
+	database.query("SELECT PAYLOAD FROM MQTT_LOG")
+	
+	#process the query results
+	for payload_dict in database.query_result:
+		#extract data and convert into a JSON string
+		var payload_json = payload_dict["PAYLOAD"]
+		
+		#convert the JSON string back into a dictionary
+		var parsed_payload = parse_json(payload_json)
+		
+		if typeof(parsed_payload) == TYPE_DICTIONARY:
+			#creates a new instance of Results class with parameters and add to array
+			var question_node = int(parsed_payload["question_node"])
+			var reference_node = int(parsed_payload["reference_node"])
+			var answer = parsed_payload["answer"]
+			var timestamp = parsed_payload["timestamp"]
+			var result = Results.new(question_node, reference_node, answer, timestamp)
+			results_array.append(result)
+		else:
+			print("Failed to parse JSON")
+	
+	#checking that array is filled		
+	for result in results_array:
+		result.print_all()
+
+
+#helper function to parse JSON
+func parse_json(json_string):
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	if error == OK:
+		return json.get_data()
+	else:
+		print("JSON parse error: ", error)
+		return null
